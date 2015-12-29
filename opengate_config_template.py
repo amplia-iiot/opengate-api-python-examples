41d467d73c5b463690c90a6cdd484a9bcbf6e381
Charlie <carlos.prados@gmail.com>
2015-12-29 13:24:00 +0100

    Config template added

---
+++ opengate_config_template.py
@@ -0,0 +1,9 @@
+OPENGATE_HOST = 'cloud.opengate.es'
+NORTH_PORT = 25281
+SOUTH_PORT = 9955
+OG_NORTH_API_BASE_URI = 'http://{0}:{1}/v70'.format(OPENGATE_HOST, NORTH_PORT)
+OG_SOUTH_API_BASE_URI = 'http://{0}:{1}/v70'.format(OPENGATE_HOST, SOUTH_PORT)
+ORGANIZATION = 'your_organization'
+CHANNEL = 'your_channel'
+API_KEY = 'your_api_key'
+DEFAULT_DEVICE_ID = 'your_default_device_id'
