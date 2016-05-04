c2a91016c133e2edf011bd5b1ed5c90ff616cc4e
Carlos Prados <carlos.prados@amplia.es>
2016-05-04 16:29:08 +0200

    New headers and WebSocket configuration

--- opengate_config-template.py
+++ opengate_config-template.py
@@ -1,16 +1,21 @@
 OPENGATE_HOST = 'cloud.opengate.es'
 NORTH_PORT = 25281
 SOUTH_PORT = 9955
+SOUTH_WEBSOCKET_PORT = 9956
 OG_NORTH_API_BASE_URI = 'http://{0}:{1}/v70'.format(OPENGATE_HOST, NORTH_PORT)
 OG_SOUTH_API_BASE_URI = 'http://{0}:{1}/v70'.format(OPENGATE_HOST, SOUTH_PORT)
+OG_SOUTH_WEBSOCKET_BASE_URI = 'ws://{0}:{1}/v70'.format(OPENGATE_HOST, SOUTH_WEBSOCKET_PORT)
 ORGANIZATION = 'your_organization'
 CHANNEL = 'default_channel'
 API_KEY = 'your_api_key'
+HEADERS = {
+    'X-ApiKey': API_KEY,
+    'Content-Type': 'application/json'
+}
 DEFAULT_DEVICE_ID = None
 DEFAULT_WIFI_ID = None
-
-BUNDLE_NAME = 'test-bundle'
-BUNDLE_VERSION = '0.0.1'
+BUNDLE_NAME = 'test-bundle-may'
+BUNDLE_VERSION = '0.0.2'
 MANUFACTURER = 'Test-Manufacturer'
 MODEL_NAME = 'Virtual-Test-Device'
-
+#################################################
